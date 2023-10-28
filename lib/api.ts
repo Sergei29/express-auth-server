f482e5a5994ab0a0a6d9ad57fbf613b9dfc22db7
Serge Basangovs <sergejs.basangovs@gmail.com>
2023-10-28 17:27:50 +0300

    feat: login and register endpoints

--- lib/api.ts
+++ lib/api.ts
@@ -1,5 +1,18 @@
+import bcryptjs from "bcryptjs";
+
 import { IUser, IUserDb } from "../types";
 import { db } from "./db";
+
+const hashPassword = async (pw: string) => {
+  const salt = await bcryptjs.genSalt(10);
+  const hashedPw = await bcryptjs.hash(pw, salt);
+
+  return hashedPw;
+};
+
+export const validatePassword = (password: string, passwordHash: string) => {
+  return bcryptjs.compare(password, passwordHash);
+};
 
 export const findUserBy = async ({
   email,
@@ -21,8 +34,17 @@ export const createNewUser = async ({
   password,
   name,
   email,
-}: { password: string } & Pick<IUser, "name" | "email">) => {
-  // const newUser = await db.user.create()
+}: { password: string } & Pick<IUser, "name" | "email">): Promise<IUserDb> => {
+  const passwordHash = await hashPassword(password);
+  const newUser = await db.user.create({
+    data: {
+      name,
+      email,
+      passwordHash,
+    },
+  });
+
+  return newUser;
 };
 
 export const formatDateToString = ({
--- routes/auth/index.ts
+++ routes/auth/index.ts
@@ -1,21 +1,68 @@
 import { Router } from "express";
 import dotenv from "dotenv";
 
+import {
+  findUserBy,
+  createNewUser,
+  formatDateToString,
+  validatePassword,
+} from "../../lib/api";
+
 dotenv.config();
 
 const authRouter = Router();
 
+authRouter.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    res.statusMessage = "Wrong payload";
+    res.status(401).end();
+    return;
+  }
+
+  const foundUserDb = await findUserBy({ email });
+  if (!foundUserDb) {
+    res.statusMessage = "User not found. Go to register";
+    res.status(403).end();
+    return;
+  }
+
+  const isPasswordMatch = await validatePassword(
+    password,
+    foundUserDb.passwordHash,
+  );
+
+  if (!isPasswordMatch) {
+    res.statusMessage = "Credentials dont match.";
+    res.status(403).end();
+    return;
+  }
+
+  const { passwordHash, ...userInfo } = formatDateToString(foundUserDb);
+  res.status(200).send({ data: userInfo });
+});
+
 authRouter.post("/register", async (req, res) => {
   const { name, email, password } = req.body;
+
   if (!name || !email || !password) {
     res.statusMessage = "Wrong payload";
     res.status(401).end();
     return;
   }
 
-  console.log("credentials register: ", { name, email, password });
+  const foundUser = await findUserBy({ email });
+  if (!!foundUser) {
+    res.statusMessage = "User already exists. Go to login";
+    res.status(403).end();
+    return;
+  }
 
-  res.status(200).send({ data: { id: Date.now().toString(), name, email } });
+  const newUserDb = await createNewUser({ email, name, password });
+
+  const { passwordHash, ...userInfo } = formatDateToString(newUserDb);
+
+  res.status(200).send({ data: userInfo });
 });
 
 export { authRouter };
